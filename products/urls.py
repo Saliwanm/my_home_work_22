fa44459606304c4c07f4e05dd6fa45460d26f0e7
saliwan <saliwanm@gmail.com>
2022-12-28 13:37:03 +0200

    first commit

---
+++ products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("/add", views.add_product, name="add_product"),
+    path("/edit/<int:id>", views.edit_product, name="edit_product"),
+    path("/update/<int:id>", views.update_product, name="update_product"),
+    path("/category", views.all_category, name="all_category"),
+    path("/category/add", views.add_category, name="add_category"),
+    path('/category/edit/<int:id>', views.edit_category, name="edit_category"),
+    path("/category/update/<int:id>", views.update_category, name="update_category"),
+    path("/<int:id>", views.product_details, name="product_details")
+]
